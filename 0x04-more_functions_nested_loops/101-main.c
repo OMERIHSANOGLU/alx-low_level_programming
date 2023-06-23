85b8e6a576105ff72f2ec49f8d5a8057432c073c
OMERIHSANOGLU <omerehsan89@gmail.com>
2023-06-22 18:54:58 -0700

    0. isupper updated

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -1,21 +1,28 @@
 #include "main.h"
+#include <limits.h>
 
 /**
- * main - check the code
+ * main - check the code for Hn alx students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
-    print_number(0);
-    _putchar('\n');
-    print_number(-98);
-    _putchar('\n');
-    return (0);
+	print_number(98);
+	_putchar('\n');
+	print_number(402);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	print_number(-1);
+	_putchar('\n');
+       print_number(INT_MAX);
+       _putchar('\n');
+       print_number(INT_MIN);
+       _putchar('\n');
+	return (0);
 }
